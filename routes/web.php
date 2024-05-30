c5057a9be469dc25f0d3e337c1c5836449cd3d90
t.nakatamari <t.nakatamari@core-tech.jp>
2024-05-30 09:54:38 +0900

    ルートのコメントアウト部分の削除

--- routes/web.php
+++ routes/web.php
@@ -41,11 +41,4 @@ Route::middleware('auth')->group(function () {
     Route::delete('/blog/mypage/{id}', [MyProfileController::class, 'destroy'])->name('myprofile.destroy');
 });
 
-// // プロフィール編集画面
-// Route::get('/blog/mypage/{id}/edit', [MyProfileController::class, 'edit'])->name('myprofile.edit');
-// // プロフィール更新
-// Route::post('/blog/mypage/{id}/edit', [MyProfileController::class, 'update'])->name('myprofile.update');
-// // アカウント削除
-// Route::delete('/blog/mypage/{id}', [MyProfileController::class, 'destroy'])->name('myprofile.destroy');
-
 require __DIR__ . '/auth.php';
