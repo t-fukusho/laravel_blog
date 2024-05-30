422fe5f425923f93084ed081974a500162107e04
t-fukusho <t.fukusho@core-tech.jp>
2024-05-30 15:10:21 +0900

    微修正

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -31,6 +31,7 @@
 
         <!-- メインコンテンツ -->
         <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
             <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                 <h1 class="h2">トップページ</h1>
                 <a href="/blog/mypage/{{ $id }}">my Page</a>
