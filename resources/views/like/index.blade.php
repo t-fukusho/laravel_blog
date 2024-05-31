86f05fe083285107a8100db24a101951856d191a
r-eguchi0112 <r.eguchi@core-tech.jp>
2024-05-31 10:02:19 +0900

    マイページ画面cssの変更

--- resources/views/like/index.blade.php
+++ resources/views/like/index.blade.php
@@ -6,7 +6,7 @@
 </div>
 <h3>いいねした記事</h3>
 <div class="flex">
-        @foreach ($likes as $like)
+    @foreach ($likes as $like)
         <ul class="article">
             <li><a href="/blog/article/{{ $like->id }}" class="link">{{ $like->title }}</a></li>
             <li>{{ $like->content }}</li>
